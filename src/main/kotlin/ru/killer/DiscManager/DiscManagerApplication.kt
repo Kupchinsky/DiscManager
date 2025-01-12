7ba225cb24b27cfd41c22b5d32bff1e84b90eeee
Vasiliy Kupchinskiy <vasiliy.kupchinskiy@gmail.com>
2025-01-12 14:57:10 +0700

    Update

---
+++ src/main/kotlin/ru/killer/DiscManager/DiscManagerApplication.kt
@@ -0,0 +1,11 @@
+package ru.killer.DiscManager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiscManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<DiscManagerApplication>(*args)
+}
---
+++ src/test/kotlin/ru/killer/DiscManager/DiscManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.killer.DiscManager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiscManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
